ded3d32413929e28911b46cf21817b790269372f
Laurent Perotin <laurent@botify.com>
2018-10-11 12:01:31 +0200

    add first tests

--- qa_about_qa/blog/urls.py
+++ qa_about_qa/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="index"),
+    path("", views.home, name="home"),
     path("category/<str:category_name>", views.view_articles_by_category, name="view_articles_by_category"),
     path("article/<slug:slug>", views.view_article, name="view_article"),
     path("contact", views.contact, name="contact"),
