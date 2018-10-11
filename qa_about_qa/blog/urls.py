80d7c20dc8c8897b3d9bbfd6ebcef1b68c315831
Laurent Perotin <laurent@botify.com>
2018-10-11 11:32:01 +0200

    Implement navigation bar

--- qa_about_qa/blog/urls.py
+++ qa_about_qa/blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="index"),
+    path("category/<str:category_name>", views.view_articles_by_category, name="view_articles_by_category"),
     path("article/<slug:slug>", views.view_article, name="view_article"),
     path("contact", views.contact, name="contact"),
 ]
--- qa_about_qa/blog/views.py
+++ qa_about_qa/blog/views.py
@@ -1,22 +1,21 @@
-from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
-from blog.models import Article
+from blog.models import Article, Category
 
+# TODO find how not to set this variable in each view
+categories = Category.objects.all()
 
 def home(request):
     articles = Article.objects.all()
-    return render(request, "blog/home.html", {"last_articles": articles})
-
-
-# def lire(request, slug):
-#     article = get_object_or_404(Article, slug=slug)
-#     return render(request, "blog/lire.html", {"article": article})
-
+    return render(request, "blog/home.html", {"last_articles": articles, "categories": categories})
 
 def view_article(request, slug):
     article = get_object_or_404(Article, slug=slug)
-    return render(request, "blog/view_article.html", {"article": article})
+    return render(request, "blog/view_article.html", {"article": article, "categories": categories})
+
+def view_articles_by_category(request, category_name):
+    articles_from_category = Article.objects.filter(category__name__contains=category_name)
+    return render(request, "blog/view_articles_by_category.html", {"articles_from_category": articles_from_category, "categories": categories})
 
 def contact(request):
-    return render(request, "blog/contact.html")
+    return render(request, "blog/contact.html", {"categories": categories})
 
